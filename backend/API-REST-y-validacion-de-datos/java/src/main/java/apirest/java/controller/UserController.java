e94941e5a7945759aaa54019f41013307a7d4482
JGRDeveloper <jgroldan.dev@gmail.com>
2024-06-07 10:49:06 -0300

    API java
    Faltan hacer las validaciones

---
+++ backend/API-REST-y-validacion-de-datos/java/src/main/java/apirest/java/controller/UserController.java
@@ -0,0 +1,42 @@
+package apirest.java.controller;
+
+import apirest.java.models.User;
+import apirest.java.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getUsers(){
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserByID(@PathVariable Long id){
+        return userService.getUserByID(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteByID(@PathVariable Long id){
+        return userService.deleteByID(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody User user){
+        return userService.updateByID(id, user);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+}
