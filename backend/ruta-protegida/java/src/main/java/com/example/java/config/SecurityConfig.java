a9224ce44f68de4f59e03dbe5d834b2e6dce9caa
JGRDeveloper <jgroldan.dev@gmail.com>
2024-08-05 16:24:16 -0300

    Generación token de sesión al loguearte.

--- backend/ruta-protegida/java/src/main/java/com/example/java/config/SecurityConfig.java
+++ backend/ruta-protegida/java/src/main/java/com/example/java/config/SecurityConfig.java
@@ -1,16 +1,24 @@
 package com.example.java.config;
 
+import com.example.java.config.JWT.JwtAuthEntryPoint;
 import com.example.java.config.JWT.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -18,7 +26,8 @@ public class SecurityConfig {
 
 //    private static final String ADMIN = "ADMIN";
 //    private static final String USER = "USER";
-
+    @Autowired
+    private JwtAuthEntryPoint unauthorizedHandler;
     @Bean
     public JwtRequestFilter authenticationJwtTokenFilter() {
         return new JwtRequestFilter();
@@ -33,30 +42,31 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource()
+    {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:8080"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+        configuration.setAllowedHeaders(List.of("Access-Control-Allow-Origin", "X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf((csrf) -> csrf
-                .ignoringRequestMatchers("/no-csrf")
-                .ignoringRequestMatchers("api/register"))
+                .ignoringRequestMatchers("/no-csrf", "api/register", "api/login"))
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling((exception)-> exception.authenticationEntryPoint(unauthorizedHandler))
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("api/hola", "api/register").permitAll()
+                        .requestMatchers("api/hola", "api/register", "api/login").permitAll()
                         .anyRequest().authenticated()
                 );
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
-//    @Bean
-//    public UserDetailsService userDetailsService() throws Exception {
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//        manager.createUser(User
-//                .withUsername("thomas@gmail.com")
-//                .password(passwordEncoder().toString())
-//                .roles(ADMIN).build());
-//        manager.createUser(User
-//                .withUsername("bill@gmail.com")
-//                .password(passwordEncoder().toString())
-//                .roles(USER).build());
-//        return manager;
-//    }
 }
