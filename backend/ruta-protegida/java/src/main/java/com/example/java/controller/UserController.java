8ff284b12d0d65c8073a98e077e5c9fb53b16107
JGRDeveloper <jgroldan.dev@gmail.com>
2024-08-05 16:24:16 -0300

    Generación token de sesión al loguearte.

--- backend/ruta-protegida/java/src/main/java/com/example/java/controller/UserController.java
+++ backend/ruta-protegida/java/src/main/java/com/example/java/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.java.controller;
 
 
 import com.example.java.config.JWT.JwtTokenUtil;
+import com.example.java.config.PAYLOAD.JwtResponse;
+import com.example.java.config.PAYLOAD.LoginRequest;
 import com.example.java.config.PAYLOAD.MessageResponse;
 import com.example.java.config.PAYLOAD.RegisterRequest;
 import com.example.java.model.User;
@@ -10,8 +12,13 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -25,7 +32,15 @@ public class UserController {
 
     @GetMapping("/any")
     public String NoAuthRequired(){
-        return "RUTA PARA CUALQUIER PERSONA.";
+        return "Ruta para cualquier persona.";
+    }
+    @GetMapping("/auth")
+    public String auth(){
+        return "Ruta para cualquier persona logueada.";
+    }
+    @GetMapping("/admin")
+    public String admin(){
+        return "Ruta para el ADMIN.";
     }
     @PostMapping("/register")
     public ResponseEntity<MessageResponse> registerUser(@RequestBody RegisterRequest signUpRequest){
@@ -46,5 +61,16 @@ public class UserController {
 
         return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
 
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody LoginRequest loginRequest){
+        Authentication authentication = authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtTokenUtil.generateJwtToken(authentication);
+
+        return ResponseEntity.ok(new JwtResponse(jwt));
     }
 }
