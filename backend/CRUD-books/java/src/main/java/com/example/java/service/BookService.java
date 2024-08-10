e7972c9bb7590c36893f1ef4247a8e11a24d71c8
JGRDeveloper <jgroldan.dev@gmail.com>
2024-08-10 17:16:06 -0300

    Nuevos metodos GET para entidad Book.

--- backend/CRUD-books/java/src/main/java/com/example/java/service/BookService.java
+++ backend/CRUD-books/java/src/main/java/com/example/java/service/BookService.java
@@ -1,13 +1,17 @@
 package com.example.java.service;
 
 import com.example.java.model.Book;
+import com.example.java.model.Genre;
 import com.example.java.repositories.IBookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 @Service
 public class BookService {
@@ -17,5 +21,19 @@ public class BookService {
 
     public ResponseEntity<List<Book>> getAllBooks(){
         return new ResponseEntity<>(bookRepository.findAll(), HttpStatus.OK);
+    }
+
+    public ResponseEntity<Optional<Book>> getBookByID(@PathVariable Integer id){
+        return new ResponseEntity<>(bookRepository.findById(id), HttpStatus.OK);
+    }
+
+    public ResponseEntity<List<Genre>> getGenreByBook(@PathVariable Integer id){
+        return new ResponseEntity<>(
+                bookRepository
+                        .findById(id)
+                        .get()
+                        .getGenres()
+                        ,
+                HttpStatus.OK);
     }
 }
