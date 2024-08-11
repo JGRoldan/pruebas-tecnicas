e7ebeca3343cb945f5fc33cb172fa84b6cb0a0d1
JGRDeveloper <jgroldan.dev@gmail.com>
2024-08-11 14:00:52 -0300

    Mejorando 'source of truth'

--- frontend/shopping-cart/src/App.jsx
+++ frontend/shopping-cart/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
 import { products as initialProducts } from './mocks/products.json'
 import { Products } from "./components/products/Products.jsx"
 import { Header } from './components/header/Header.jsx'
 import { useFilters } from './hooks/useFilters.jsx'
 
 function App() {
-  const [products] = useState(initialProducts)
   const { filterProducts } = useFilters()
-
-  const filteredProducts = filterProducts(products)
+  const filteredProducts = filterProducts(initialProducts)
 
   return (
     <>
--- frontend/shopping-cart/src/context/filters.jsx
+++ frontend/shopping-cart/src/context/filters.jsx
@@ -1,7 +1,10 @@
 import { createContext, useState } from "react";
 
+//Esto es lo que tenemos que consumir. 
+//Solo se crea una  vez
 export const FiltersContext = createContext();
 
+//Esto es lo que nos provee el acceso al contexto
 export const FiltersProvider = ({ children }) => {
     const [filters, setFilters] = useState({category: 'all', minPrice: 0})
 
