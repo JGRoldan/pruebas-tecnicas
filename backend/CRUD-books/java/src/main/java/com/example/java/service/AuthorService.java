d15c06a66cd7f6c2289fe3c709c9095c34d60341
JGRDeveloper <jgroldan.dev@gmail.com>
2024-08-17 15:35:44 -0300

    Customizando ApiResponse.

--- backend/CRUD-books/java/src/main/java/com/example/java/service/AuthorService.java
+++ backend/CRUD-books/java/src/main/java/com/example/java/service/AuthorService.java
@@ -1,5 +1,6 @@
 package com.example.java.service;
 
+import com.example.java.Response.ApiResponse;
 import com.example.java.model.Author;
 import com.example.java.repositories.IAuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,34 +19,49 @@ public class AuthorService {
     @Autowired
     private IAuthorRepository authorRepository;
 
-    public ResponseEntity<List<Author>> getAllAuthors(){
-        return new ResponseEntity<>(authorRepository.findAll(), HttpStatus.OK);
+    public ResponseEntity<ApiResponse<List<Author>>> getAllAuthors(){
+        List<Author> authors = authorRepository.findAll();
+        ApiResponse<List<Author>> response =
+                new ApiResponse<>(HttpStatus.OK.value(),"Autores obtenidos con éxito", authors);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    public ResponseEntity<Author> getAuthorByID(@PathVariable Integer id){
-        Optional<Author> author = authorRepository.findById(id);
-        if(author.isEmpty()){
-            throw new RuntimeException("Error al encontrar el autor con ID=" + id);
+    public ResponseEntity<ApiResponse<Author>> getAuthorByID(@PathVariable Integer id){
+        Optional<Author> optionalAuthor = authorRepository.findById(id);
+        if(optionalAuthor.isPresent()){
+            Author author =  optionalAuthor.get();
+            ApiResponse<Author> response = new ApiResponse<>(HttpStatus.OK.value(), "Autor encontrado exitosamente con id: " + id, author);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } else {
+            ApiResponse<Author> response = new ApiResponse<>(HttpStatus.NOT_FOUND.value(), "Autor no encontrado con id: " + id, null);
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(author.get(), HttpStatus.OK);
     }
 
-    public ResponseEntity<Author> deleteAuthorByID(@PathVariable Integer id){
+    public ResponseEntity<ApiResponse<Author>> deleteAuthorByID(@PathVariable Integer id){
         Optional<Author> author = authorRepository.findById(id);
-        if(author.isEmpty()){
-            throw new RuntimeException("Error al encontrar el autor con ID=" + id);
+
+        if(author.isPresent()){
+            Author deletedAuthor =  author.get();
+            authorRepository.deleteById(id);
+
+            ApiResponse<Author> response = new ApiResponse<>(HttpStatus.OK.value(), "Autor borrado exitosamente.", deletedAuthor);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } else {
+            ApiResponse<Author> response = new ApiResponse<>(HttpStatus.NOT_FOUND.value(), "Autor no encontrado con id: " + id, null);
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
         }
-        authorRepository.deleteById(id);
-        return new ResponseEntity<>(author.get(), HttpStatus.OK);
     }
 
-    public ResponseEntity<Author> updateAuthorByID(@PathVariable Integer id, @RequestBody Author author) {
+    public ResponseEntity<ApiResponse<Author>> updateAuthorByID(@PathVariable Integer id, @RequestBody Author author) {
         Optional<Author> auth = null;
         try {
             auth = authorRepository.findById(id);
 
             if (auth.isEmpty()) {
-                throw new RuntimeException("Error al encontrar el autor con ID=" + id);
+                ApiResponse<Author> response = new ApiResponse<>(HttpStatus.NOT_FOUND.value(), "Autor no encontrado con id: " + id, null);
+                return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
             }
 
             Author existingAuthor = auth.get();
@@ -53,15 +69,22 @@ public class AuthorService {
             existingAuthor.setLast_name(author.getLast_name());
 
             authorRepository.save(existingAuthor);
-            return new ResponseEntity<>(auth.get(), HttpStatus.OK);
+
+            ApiResponse<Author> response = new ApiResponse<>(HttpStatus.CREATED.value(), "Autor actualizado exitosamente con id: " + id, existingAuthor);
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<>(auth.get(), HttpStatus.INTERNAL_SERVER_ERROR);
+            ApiResponse<Author> response =
+                    new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage() , null);
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    public ResponseEntity<Author> createAuthor(@RequestBody Author author) {
+    public ResponseEntity<ApiResponse<Author>> createAuthor(@RequestBody Author author) {
         Author savedAuthor = authorRepository.save(author);
-        return new ResponseEntity<>(savedAuthor, HttpStatus.CREATED);
+        ApiResponse<Author> response =
+                new ApiResponse<>(HttpStatus.CREATED.value(), "Autor creado exitosamente.", savedAuthor);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 }
