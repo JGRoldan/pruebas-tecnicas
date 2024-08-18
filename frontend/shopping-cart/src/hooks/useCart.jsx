7686d8b2a99e3f7783fe29915fd6b7bcc7d0ff3c
JGRDeveloper <jgroldan.dev@gmail.com>
2024-08-18 15:26:34 -0300

    CartContext

---
+++ frontend/shopping-cart/src/hooks/useCart.jsx
@@ -0,0 +1,12 @@
+import {  useContext } from 'react'
+import { CartContext } from '../context/cart'
+
+export function useCart(){
+    const context = useContext(CartContext)
+  
+    if(context === undefined){
+        throw new Error('useCart debe ser usado dentro de un CartProvider.')
+    }
+  
+    return context
+  }
