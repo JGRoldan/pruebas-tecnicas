b8d45d267ec74241aab17b5adcea85aa79d91145
JGRDeveloper <jgroldan.dev@gmail.com>
2024-08-22 15:03:34 -0300

    Agregando cambios al agregar elementos.

--- frontend/shopping-cart/src/components/products/Products.jsx
+++ frontend/shopping-cart/src/components/products/Products.jsx
@@ -1,15 +1,21 @@
 import './Products.css'
-import { AddToCartIcon } from '../icon/Icons.jsx'
+import { AddToCartIcon, RemoveFromCartIcon } from '../icon/Icons.jsx'
 import { useCart } from '../../hooks/useCart.jsx'
 
 export function Products({ products }) {
-    const { cart, addToCart } = useCart()
-    console.log(cart)
+    const { cart, addToCart, removeFromCart } = useCart()
+
+    const checkProductInCart = (product) =>{
+        return cart.some(item => item.id === product.id)
+    }
+
     return (
         <main className='products'>
             <ul>
-                {products.slice(0,10).map(product =>
-                    (
+                {products.slice(0,10).map(product =>{
+
+                    const isProductInCart = checkProductInCart(product)
+                    return(
                         <li key={product.id}>
                             <img src={product.thumbnail} alt={product.description} />
                             <div>
@@ -18,12 +24,16 @@ export function Products({ products }) {
                             <div>
                                 ${product.price}
                             </div>
-                            <button onClick={()=> addToCart(product)}>
-                                <AddToCartIcon />
+                            <button
+                                style={{backgroundColor: isProductInCart ? 'red' : '#09f'}}  
+                                onClick={()=> isProductInCart ? removeFromCart(product) : addToCart(product)}>
+                                {
+                                    isProductInCart ? <RemoveFromCartIcon /> : <AddToCartIcon />
+                                }
                             </button>
                         </li>
                     )
-                )}
+                })}
             </ul>
         </main>
     )
