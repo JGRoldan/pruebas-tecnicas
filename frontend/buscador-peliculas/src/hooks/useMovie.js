a9004b41e3d41e280ae6082f5df2fce245c9c6ee
JGRoldan <jgroldan.dev@gmail.com>
2024-09-11 10:39:37 -0300

    Se agrega services.

--- frontend/buscador-peliculas/src/hooks/useMovie.js
+++ frontend/buscador-peliculas/src/hooks/useMovie.js
@@ -1,28 +1,13 @@
 import { useState } from 'react'
-import withResponseMovies from '../mocks/response.json'
-import noResponseMovies from '../mocks/no-response.json'
+import { searchMovie } from '../services/movies'
 
 export function useMovies({ search }){
-    const [responseMovie, setResponseMovie] = useState([])
-    const movies = responseMovie.Search
-    const mappedMovies = movies?.map(movie =>({
-        title: movie.Title,
-        year: movie.Year,
-        poster: movie.Poster,
-        id: movie.imdbID
-      })
-    )
+    const [movies, setMovies] = useState([])
 
-    const getMovies = () =>{
-      if(search) {
-        const API_KEY = import.meta.env.VITE_API_KEY
-        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
-          .then(response => response.json())
-          .then(data => setResponseMovie(data))
-          .catch(error => console.log(error))
-      }
-      else {setResponseMovie(noResponseMovies)}
+    const getMovies = async () =>{
+        const movies = await searchMovie({ search })
+        setMovies(movies)
     }
 
-    return { movies: mappedMovies, getMovies }
+    return { movies, getMovies }
 }
