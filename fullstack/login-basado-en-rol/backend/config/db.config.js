156a050a6dd7e2d59a2d172dff91d1ae4d55186b
JGRDeveloper <jgroldan.dev@gmail.com>
2024-12-03 20:39:17 -0300

    Fix production

--- fullstack/login-basado-en-rol/backend/config/db.config.js
+++ fullstack/login-basado-en-rol/backend/config/db.config.js
@@ -12,6 +12,7 @@ export const sequelize = new Sequelize({
     password: process.env.PASSWORD,
     host: process.env.HOST,
     port: process.env.DB_PORT,
+    dialectModule: require('mysql2'),
 })
 
 const db = {}
